03db8a014f4d4c40d0e45d28519c7220778bc8a5
unknown <tungdao923@gmail.com>
2022-08-12 00:40:20 +0700

    insertion Sort

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import jdk.internal.dynalink.linker.LinkerServices;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element: ");
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int currentElement = arr[i];
+            int k;
+            for ( k = i -1; k >= 0 && arr[k] > currentElement ; k--) {
+                arr[k + 1] = arr[k];
+            }
+            arr[k + 1] = currentElement;
+        }
+    }
+}
